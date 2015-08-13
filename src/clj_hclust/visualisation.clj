69995e0942b32c3dc6a86c8fcf4815fef608b2d9
Romain Leroux <leroux.rgd@gmail.com>
2015-08-13 22:15:58 +0900

    improved hclust->points

--- src/clj_hclust/visualisation.clj
+++ src/clj_hclust/visualisation.clj
@@ -1,5 +1,6 @@
 (ns clj-hclust.visualisation
-  (:require [analemma.svg :as svg]
+  (:require [hiccup.core :as h] 
+            [analemma.svg :as svg]
             [analemma.xml :as xml]
             [analemma.charts :as charts])
   (:import [javax.swing JFrame] 
@@ -7,7 +8,7 @@
            [org.apache.batik.swing JSVGCanvas]                                 
            [org.apache.batik.util XMLResourceDescriptor]))
 
-;; TODO hclust->newick
+;; TODO hclust->newick (using "format" from postwalk?)
 (comment
   [[[0 0 0] [1 1 0] 0.5] [[[2 2 0] [3 3 0] 1.12] [4 4 0] 1.41] 2.24]
   "(((0:0,1:0):0.5,((2:0,3:0):1.12,4:0):1.41):2.24);")
@@ -30,7 +31,7 @@
     (java.awt.EventQueue/invokeLater
      (proxy [Runnable] []
        (run []
-         (doto (JFrame. "My Awesome SVG")
+         (doto (JFrame. "Displaying SVG")
            (.. (getContentPane) (add svgcanvas))
            (.setSize 600 600)
            (.setVisible true)))))))
@@ -52,48 +53,55 @@
   (let [dmax (last clustering)
         nb-leaves (atom 0)
         bary-stack (java.util.ArrayDeque.)
-        points (atom [])]
+        points (atom (transient []))]
     (clojure.walk/postwalk
-     (fn [f]
-       (when (sequential? f)
-         (let [l (nth f 0) r (nth f 1) d (nth f 2) 
-               shape-l (shape l) shape-r (shape r)]
+     (fn [elem]
+       (when (sequential? elem)
+         (let [[l r d] elem ; all sequential elements match [left right distance]
+               shape-l (shape l) 
+               shape-r (shape r)]
            (cond
-             ;; merge two leaves
+             ;; elem is the merge of two leaves
              (and (= :leaf shape-l) (= :leaf shape-r))
-             (do (swap! points conj {:type :leaf :id (first l) 
-                                     :x xmax :y (* rad (+ 1 (* 2 @nb-leaves)))})
-                 (swap! nb-leaves + 1)
-                 (swap! points conj {:type :leaf :id (first r) 
-                                     :x xmax :y (* rad (+ 1 (* 2 @nb-leaves)))})
-                 (swap! nb-leaves + 1)
-                 (let [center (* 2 (- @nb-leaves 1) rad)
-                       y-up (- center rad)
-                       y-down (+ center rad)]
-                   (swap! points conj {:type :fork :span [y-up y-down]
-                                       :x (double (* xmax (- 1 (/ d dmax)))) :y center})
-                   (.push bary-stack center)))
-             ;; merge cluster and one leaf
+             (let [y-leaf1 (* rad (+ 1 (* 2 @nb-leaves)))
+                   y-leaf2 (+ y-leaf1 (* 2 rad))
+                   center (+ y-leaf1 rad)
+                   x-fork (* xmax (- 1. (/ d dmax)))]
+               (swap! points 
+                      #(-> %
+                           (conj! {:type :leaf :id (first l) :x xmax :y y-leaf1})
+                           (conj! {:type :leaf :id (first r) :x xmax :y y-leaf2})                 
+                           (conj! {:type :fork :span [y-leaf1 y-leaf2] :x x-fork :y center})))
+               (swap! nb-leaves + 2)
+               (.push bary-stack center))
+                         
+             ;; elem is the merge of a cluster and a leaf
              (and (= :cluster shape-l) (= :leaf shape-r))
-             (do (swap! points conj {:type :leaf :id (first r) 
-                                     :x xmax :y (* rad (+ 1 (* 2 @nb-leaves)))})
-                 (swap! nb-leaves + 1)
-                 (let [y-up (.poll bary-stack)
-                       center (+ (* 2 rad) y-up)
-                       y-down (+ center rad)]
-                   (swap! points conj {:type :fork :span [y-up y-down]
-                                       :x (double (* xmax (- 1 (/ d dmax)))) :y center})
-                   (.push bary-stack center)))
-             ;; merge two clusters
+             (let [y-up (.poll bary-stack)                       
+                   y-down (* rad (+ 1 (* 2 @nb-leaves)))
+                   center (/ (+ y-up y-down) 2.)
+                   x-fork (* xmax (- 1. (/ d dmax)))]
+               (swap! points
+                      #(-> %
+                           (conj! {:type :leaf :id (first r) :x xmax :y y-down})
+                           (conj! {:type :fork :span [y-up y-down] :x x-fork :y center})))
+               (swap! nb-leaves + 1)
+               (.push bary-stack center))
+             
+             ;; elem is the merge of two clusters
              (and (= :cluster shape-l) (= :cluster shape-r))
              (let [y-up (.poll bary-stack)
                    y-down (.poll bary-stack)
-                   center (/ (+ y-up y-down) 2.)] 
-               (swap! points conj {:type :fork :span [y-up y-down]
-                                   :x (double (* xmax (- 1 (/ d dmax)))) :y center})
+                   center (/ (+ y-up y-down) 2.)
+                   x-fork (* xmax (- 1. (/ d dmax)))]
+               (swap! points 
+                      #(conj! % {:type :fork :span [y-up y-down] :x x-fork :y center}))
                (.push bary-stack center)))))
-       f)
+       elem)
      clustering)
-    @points))
-(hclust->points C {:rad 5 :xmax 500})
+    (persistent! @points)))
+#_(hclust->points C {:rad 5 :xmax 500})
+
+(defn points->svg-hiccup [points]
+  )
 
