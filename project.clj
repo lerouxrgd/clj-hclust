59415d95b880e7b38805b646b01a0114b5d761c3
Leroux, Romain | Raru | BDD <romain.leroux@mail.rakuten.com>
2015-08-24 22:56:20 +0900

    commit for 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-hclust "0.1.0-SNAPSHOT"
+(defproject clj-hclust "0.1.0"
   :description "Hierarchical clustering and visualisation for clojure"
   :url "https://github.com/lerouxrgd/clj-hclust"
   :license {:name "Eclipse Public License"
