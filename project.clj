15734624792487bb97f92edfadb5239e4c3ae491
lerouxrgd <leroux.rgd@gmail.com>
2016-05-12 23:27:42 +0900

    mark 0.1.2 and update dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clj-hclust "0.1.2-SNAPSHOT"
+(defproject clj-hclust "0.1.2"
   :description "Hierarchical clustering and visualisation for clojure"
   :url "https://github.com/lerouxrgd/clj-hclust"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.mikera/core.matrix "0.49.0"]
-                 [net.mikera/vectorz-clj "0.43.1"]
-                 [medley "0.7.2"]
+                 [net.mikera/core.matrix "0.52.0"]
+                 [net.mikera/vectorz-clj "0.44.0"]
+                 [medley "0.8.1"]
                  [hiccup "1.0.5"]
                  [org.clojars.pallix/batik "1.7.0"]])
 
